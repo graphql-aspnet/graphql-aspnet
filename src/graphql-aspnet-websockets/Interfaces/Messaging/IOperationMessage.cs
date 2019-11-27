bdfd2f848b587524cb17c1d26b19b50164b60038
Kevin Carroll <kevin.carroll@outlook.com>
2019-11-27 09:12:47 -0700

    remove erroneous modifiers from interface.

--- src/graphql-aspnet-websockets/Interfaces/Messaging/IOperationMessage.cs
+++ src/graphql-aspnet-websockets/Interfaces/Messaging/IOperationMessage.cs
@@ -20,18 +20,18 @@ namespace GraphQL.AspNet.Interfaces.Messaging
         /// Gets or sets the payload of the message as a stringified json object.
         /// </summary>
         /// <value>The payload.</value>
-        public string Payload { get; set; }
+        string Payload { get; set; }
 
         /// <summary>
         /// Gets or sets the identifier for the scoped operation started by a client.
         /// </summary>
         /// <value>The identifier.</value>
-        public string Id { get; set; }
+        string Id { get; set; }
 
         /// <summary>
         /// Gets or sets the type of the message, indicating expected payload types.
         /// </summary>
         /// <value>The type.</value>
-        public OperationMessageType Type { get; set; }
+        OperationMessageType Type { get; set; }
     }
 }
