c1bfbfd16d1c7f5dc759a9f734e66b94df2caa44
Kevin Carroll <kevin.carroll@outlook.com>
2019-12-05 15:29:00 -0700

    initial websocket configuration interfaces

---
+++ src/graphql-aspnet-websockets/Configuration/Mvc/GraphQLMvcSchemaWebSocketBuilderExtensions.cs
@@ -0,0 +1,40 @@
+﻿// *************************************************************
+// project:  graphql-aspnet
+// --
+// repo: https://github.com/graphql-aspnet
+// docs: https://graphql-aspnet.github.io
+// --
+// License:  MIT
+// *************************************************************
+
+namespace GraphQL.AspNet.Configuration.Mvc
+{
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.Extensions.DependencyInjection;
+
+    /// <summary>
+    /// A set of extensions to configure web socket support at startup.
+    /// </summary>
+    public static class GraphQLMvcSchemaWebSocketBuilderExtensions
+    {
+        /// <summary>
+        /// Configures and injects web socket support for GraphQL ASP.NET enabling subscriptions
+        /// on this server.  This method will also attempt to call "UseWebSockets" and assign all
+        /// applicable configuration options.
+        /// </summary>
+        /// <param name="app">The application.</param>
+        /// <param name="enableSubscriptions">if set to <c>true</c> websocket support will be
+        /// added to the server and subscription support will be enabled for all
+        /// schemas.</param>
+        public static void UseGraphQL(this IApplicationBuilder app, bool enableSubscriptions = false)
+        {
+            if (enableSubscriptions)
+            {
+                app.UseWebSockets();
+            }
+
+            var serviceProvider = app.ApplicationServices;
+            serviceProvider.UseGraphQL();
+        }
+    }
+}
---
+++ src/graphql-aspnet-websockets/Configuration/SchemaSocketsConfiguration.cs
@@ -0,0 +1,35 @@
+﻿// *************************************************************
+// project:  graphql-aspnet
+// --
+// repo: https://github.com/graphql-aspnet
+// docs: https://graphql-aspnet.github.io
+// --
+// License:  MIT
+// *************************************************************
+
+namespace GraphQL.AspNet.Configuration
+{
+    using System.Diagnostics;
+    using GraphQL.AspNet.Interfaces.Configuration;
+
+    /// <summary>
+    /// A set of configuration options available for setting up your websocket support for
+    /// subscriptions for your server. These settings apply universally to all schemas supporting subscriptions.
+    /// </summary>
+    [DebuggerDisplay("Schema Declaration Configuration")]
+    public class SchemaSocketsConfiguration : ISchemaSocketsConfiguration
+    {
+        /// <summary>
+        /// Gets or sets the interval, in milliseconds, that the server will send
+        /// ping data to a connected client to keep proxies open (Default: 2 minutes).
+        /// </summary>
+        /// <value>The keep alive interval in milliseconds.</value>
+        public int KeepAliveIntervalInMilliseconds { get; set; } = 2 * 60 * 1000;
+
+        /// <summary>
+        /// Gets or sets the size of the data receive buffer (Default: 4kb).
+        /// </summary>
+        /// <value>The size of the receive buffer.</value>
+        public int ReceiveBufferSize { get; set; } = 4 * 1024;
+    }
+}
