9f104a3f9be200c40a647284ab300362faa21f7a
Kevin Carroll <kevin.carroll@outlook.com>
2021-10-05 06:39:47 -0700

    added missing file header for stylecop

--- src/tests/graphql-aspnet-testframework/CommonHelpers/TwoPropertyGenericObject.cs
+++ src/tests/graphql-aspnet-testframework/CommonHelpers/TwoPropertyGenericObject.cs
@@ -1,11 +1,14 @@
-﻿namespace GraphQL.AspNet.Tests.Framework.CommonHelpers
-{
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
+﻿// *************************************************************
+// project:  graphql-aspnet
+// --
+// repo: https://github.com/graphql-aspnet
+// docs: https://graphql-aspnet.github.io
+// --
+// License:  MIT
+// *************************************************************
 
+namespace GraphQL.AspNet.Tests.Framework.CommonHelpers
+{
     /// <summary>
     /// A test object that has two properties, both generic.
     /// </summary>
