2859582008d340f015b07913d8247bb42db1fcf9
Kevin Carroll <kevin.carroll@outlook.com>
2022-09-24 15:46:24 -0700

    WIP, added unit tests for missing desc data on introspection

---
+++ src/tests/graphql-aspnet-tests/Execution/IntrospectionTestData/TypeExtensionDescriptionController.cs
@@ -0,0 +1,36 @@
+﻿// *************************************************************
+// project:  graphql-aspnet
+// --
+// repo: https://github.com/graphql-aspnet
+// docs: https://graphql-aspnet.github.io
+// --
+// License:  MIT
+// *************************************************************
+
+namespace GraphQL.AspNet.Tests.Execution.IntrospectionTestData
+{
+    using GraphQL.AspNet.Attributes;
+    using GraphQL.AspNet.Controllers;
+    using GraphQL.AspNet.Tests.Framework.CommonHelpers;
+    using NUnit.Framework;
+
+    public class TypeExtensionDescriptionController : GraphController
+    {
+        [QueryRoot]
+        public TwoPropertyObject RetrieveObject(string prop1Value)
+        {
+            return new TwoPropertyObject()
+            {
+                Property1 = prop1Value,
+                Property2 = prop1Value.Length,
+            };
+        }
+
+        [Description("Property3 is a boolean")]
+        [TypeExtension(typeof(TwoPropertyObject), "property3", typeof(bool))]
+        public bool TwoProp_Property3(TwoPropertyObject obj)
+        {
+            return obj.Property1 == "bob";
+        }
+    }
+}
