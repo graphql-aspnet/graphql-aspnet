ed2d7d7f5bb5af80ff3797f0833418d2cf62c096
Kevin Carroll <kevin.carroll@outlook.com>
2023-05-20 11:27:56 -0700

    WIP, reconfigured csproj files

---
+++ src/unit-tests/graphql-aspnet-subscriptions-tests/Engine/TestData/FakeWebSocketManager.cs
@@ -0,0 +1,29 @@
+﻿// *************************************************************
+// project:  graphql-aspnet
+// --
+// repo: https://github.com/graphql-aspnet
+// docs: https://graphql-aspnet.github.io
+// --
+// License:  MIT
+// *************************************************************
+
+namespace GraphQL.AspNet.Tests.Engine.TestData
+{
+    using System.Collections.Generic;
+    using System.Net.WebSockets;
+    using System.Threading.Tasks;
+    using GraphQL.AspNet.Tests.Mocks;
+    using Microsoft.AspNetCore.Http;
+
+    public class FakeWebSocketManager : WebSocketManager
+    {
+        public override bool IsWebSocketRequest => true;
+
+        public override IList<string> WebSocketRequestedProtocols => new List<string>();
+
+        public override Task<WebSocket> AcceptWebSocketAsync(string subProtocol)
+        {
+            return Task.FromResult(new FakeWebSocket(WebSocketState.Open) as WebSocket);
+        }
+    }
+}
