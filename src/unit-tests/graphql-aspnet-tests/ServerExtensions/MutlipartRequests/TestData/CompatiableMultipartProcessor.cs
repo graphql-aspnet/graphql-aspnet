494af221234d9c1d38b80bfca17e9f161ebbf136
Kevin Carroll <kevin.carroll@outlook.com>
2023-06-10 13:15:10 -0700

    Bug Fix: Multipart processor type checks (#116)
    * Bug fix with configuration of multipart request extension

---
+++ src/unit-tests/graphql-aspnet-tests/ServerExtensions/MutlipartRequests/TestData/CompatiableMultipartProcessor.cs
@@ -0,0 +1,31 @@
+﻿// *************************************************************
+// project:  graphql-aspnet
+// --
+// repo: https://github.com/graphql-aspnet
+// docs: https://graphql-aspnet.github.io
+// --
+// License:  MIT
+// *************************************************************
+
+namespace GraphQL.AspNet.Tests.ServerExtensions.MutlipartRequests.TestData
+{
+    using GraphQL.AspNet.Interfaces.Engine;
+    using GraphQL.AspNet.Interfaces.Logging;
+    using GraphQL.AspNet.Interfaces.Schema;
+    using GraphQL.AspNet.ServerExtensions.MultipartRequests.Engine;
+    using GraphQL.AspNet.ServerExtensions.MultipartRequests.Interfaces;
+
+    public class CompatiableMultipartProcessor<TSchema> : MultipartRequestGraphQLHttpProcessor<TSchema>
+        where TSchema : class, ISchema
+    {
+        public CompatiableMultipartProcessor(
+            TSchema schema,
+            IGraphQLRuntime<TSchema> runtime,
+            IQueryResponseWriter<TSchema> writer,
+            IMultiPartHttpFormPayloadParser<TSchema> parser,
+            IGraphEventLogger logger = null)
+            : base(schema, runtime, writer, parser, logger)
+        {
+        }
+    }
+}
