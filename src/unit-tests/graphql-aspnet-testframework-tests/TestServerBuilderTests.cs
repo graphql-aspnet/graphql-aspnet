f6b30d9b2ba0662934bb65e64c9139327c985aa4
Freek Gruntjes <freek@hipex.io>
2023-11-10 10:17:36 +0100

    Added test case for race condition when building test server #134
    * Added graphql-aspnet-testframework-tests project with
      TestServerBuilderTests

---
+++ src/unit-tests/graphql-aspnet-testframework-tests/TestServerBuilderTests.cs
@@ -0,0 +1,47 @@
+namespace GraphQL.AspNet.TestFramework.Tests
+{
+    using System.Threading.Tasks;
+    using GraphQL.AspNet.Attributes;
+    using GraphQL.AspNet.Controllers;
+    using GraphQL.AspNet.Schemas;
+    using GraphQL.AspNet.Tests.Framework;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class TestServerBuilderTests
+    {
+        [Test]
+        public void ParallelBuildSameControllerTest()
+        {
+            Task.WaitAll(
+                Task.Run(BuildServer<AppController>),
+                Task.Run(BuildServer<AppController>),
+                Task.Run(BuildServer<AppController>),
+                Task.Run(BuildServer<AppController>),
+                Task.Run(BuildServer<AppController>));
+        }
+
+        [GraphRoute("with-param")]
+        public class AppController : GraphController
+        {
+            [Query("get")]
+            public string Get(string id)
+            {
+                return id;
+            }
+        }
+
+        private void BuildServer<T>()
+            where T : GraphController
+        {
+            var builder = new TestServerBuilder<GraphSchema>();
+
+            builder.AddGraphQL(options =>
+            {
+                options.AddController<T>();
+            });
+
+            builder.Build();
+        }
+    } 
+}
