5cd17260dd1242845fd61bf798cb9f219c95f3e2
Kevin Carroll <kevin.carroll@outlook.com>
2024-03-10 10:36:33 -0700

    Fixed self referencing complex input object lists (#152)

---
+++ src/unit-tests/graphql-aspnet-tests/Execution/TestData/ExecutionPlanTestData/SelfReferencingInputObject.cs
@@ -0,0 +1,20 @@
+﻿// *************************************************************
+//  project:  graphql-aspnet
+//  --
+//  repo: https://github.com/graphql-aspnet
+//  docs: https://graphql-aspnet.github.io
+//  --
+//  License:  MIT
+//  *************************************************************
+
+namespace GraphQL.AspNet.Tests.Execution.TestData.ExecutionPlanTestData
+{
+    using System.Collections.Generic;
+
+    public class SelfReferencingInputObject
+    {
+        public string Name { get; set; }
+
+        public IEnumerable<SelfReferencingInputObject> Children { get; set; }
+    }
+}
---
+++ src/unit-tests/graphql-aspnet-tests/Execution/TestData/ExecutionPlanTestData/SelfReferencingInputObjectController.cs
@@ -0,0 +1,38 @@
+﻿// *************************************************************
+//  project:  graphql-aspnet
+//  --
+//  repo: https://github.com/graphql-aspnet
+//  docs: https://graphql-aspnet.github.io
+//  --
+//  License:  MIT
+//  *************************************************************
+
+namespace GraphQL.AspNet.Tests.Execution.TestData.ExecutionPlanTestData
+{
+    using System.Collections.Generic;
+    using GraphQL.AspNet.Attributes;
+    using GraphQL.AspNet.Controllers;
+
+    public class SelfReferencingInputObjectController : GraphController
+    {
+        [QueryRoot]
+        public int CountNestings(SelfReferencingInputObject item)
+        {
+            var i = 0;
+            var stack = new Stack<SelfReferencingInputObject>();
+            stack.Push(item);
+            while (stack.Count > 0)
+            {
+                var curItem = stack.Pop();
+                i++;
+                if (curItem.Children != null)
+                {
+                    foreach (var child in curItem.Children)
+                        stack.Push(child);
+                }
+            }
+
+            return i;
+        }
+    }
+}
